e83a6de55cd3442e6c71f9e16c8de22b26138909
sajdkabir <sajda.kbir@gmail.com>
2025-01-03 17:15:35 +0530

    feat: linear intri

---
+++ apps/app/src/controllers/lib/linear.controller.js
@@ -0,0 +1,21 @@
+import { getAccessToken } from "../../services/lib/linear.service.js";
+
+export const getAccessTokenController = async (req, res, next) => {
+    const { code } = req.query;
+    const user = req.user;
+    if (!code || !user) {
+        return res
+            .status(400)
+            .json({ error: "Authorization code or user information is missing." });
+    }
+    try {
+        const accessToken = await getAccessToken(code, user);
+        // const userInfo = await fetchUserInfo(accessToken, user);
+
+        res.status(200).json({
+            accessToken
+        });
+    } catch (err) {
+        next(err);
+    }
+};
---
+++ apps/app/src/services/lib/linear.service.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { environment } from "../../loaders/environment.loader.js";
+
+export const getAccessToken = async (code, user) => {
+    try {
+        console.log("hmm linear code: ", code)
+        const requestBody = {
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: environment.LINEAR_REDIRECT_URL,
+            client_id: environment.LINEAR_CLIENT_ID,
+            client_secret: environment.LINEAR_CLIENT_SECRET
+        };
+
+        const tokenResponse = await axios.post('https://api.linear.app/oauth/token', requestBody, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        const accessToken = tokenResponse.data.access_token;
+
+        // user.integration.linear.accessToken = accessToken;
+        // await user.save();
+
+        return accessToken;
+    } catch (error) {
+        console.error('Error fetching Linear token:', error.response ? error.response.data : error.message);
+        throw error;
+    }
+};

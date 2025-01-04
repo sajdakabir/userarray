10e6f113ccee8ba96ac1b8e072d4ff7b6899e5f4
sajdkabir <sajda.kbir@gmail.com>
2025-01-05 02:58:36 +0530

    feat: get issue by id

--- apps/app/src/controllers/lib/issue.controller.js
+++ apps/app/src/controllers/lib/issue.controller.js
@@ -1,9 +1,23 @@
-import { getAllIssues } from "../../services/lib/issue.service.js"
+import { getAllIssues, getIssue } from "../../services/lib/issue.service.js"
 
 export const getAllIssuesController = async (req, res, next) => {
+   try {
     const workspace = res.locals.workspace
     const { team } = req.params
     const issues = await getAllIssues(workspace, team)
     res.status(200).json({ issues })
+   } catch (err) {
+    next(err)
+   }
+}
 
+export const getIssueController = async (req, res, next) => {
+   try {
+    const workspace = res.locals.workspace
+    const { team, issue:id } = req.params
+    const issue = await getIssue(workspace, team, id)
+    res.status(200).json({ issue })
+   } catch (err) {
+    next(err)
+   }
 }
--- apps/app/src/services/lib/issue.service.js
+++ apps/app/src/services/lib/issue.service.js
@@ -7,3 +7,12 @@ export const getAllIssues = async (workspace, team) => {
    })
    return issues
 }
+
+export const getIssue = async (workspace, team, id) => {
+   const issue = await Issue.findOne({
+      workspace: workspace._id,
+      team: team,
+      uuid: id
+   })
+   return issue
+}

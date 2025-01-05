35aba2c2018a33946271b79e759f2a736f227cda
sajdkabir <sajda.kbir@gmail.com>
2025-01-05 21:46:02 +0530

    fix: update user validation

--- apps/app/src/payloads/core/user.payload.js
+++ apps/app/src/payloads/core/user.payload.js
@@ -5,7 +5,8 @@ const allTimezones = moment.tz.names();
 const USER_TIMEZONE_CHOICES = allTimezones.map(timezone => timezone);
 
 const UpdateUserPayload = Joi.object({
-    fullName: Joi.string().optional(),
+    firstName: Joi.string().optional(),
+    lastName: Joi.string().optional(),
     userName: Joi.string().optional(),
     avatar: Joi.string().optional(),
     onboarding: Joi.object({

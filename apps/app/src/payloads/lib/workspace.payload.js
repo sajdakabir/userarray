454d4495f2e48683e4f4b7cfc4ac67e22818fa73
manashbarai <manashjbarai@gmail.com>
2025-01-06 05:37:35 +0530

    comment out website required to save name and slug on workspace

--- apps/app/src/payloads/lib/workspace.payload.js
+++ apps/app/src/payloads/lib/workspace.payload.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const CreateWorkspacePayload = Joi.object({
     name: Joi.string().required(),
     slug: Joi.string().required(),
-    website: Joi.string().required()
+    // website: Joi.string().required()
 });
 
 const UpdateWorkspacePayload = Joi.object({

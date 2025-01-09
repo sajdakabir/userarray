6e10d261dd436386413a7888906f8935bccc0872
sajdkabir <sajda.kbir@gmail.com>
2025-01-09 10:49:18 +0530

    feat: get public team

--- apps/app/src/routers/lib/public.route.js
+++ apps/app/src/routers/lib/public.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getWorkspacePublicProfileController } from "../../controllers/lib/workspace.controller.js";
+import { getAllPublicTeamsController } from "../../controllers/lib/team.controller.js";
 
 const router = Router();
 
 router.route("/workspaces/").get(getWorkspacePublicProfileController)
+router.route("/workspaces/:workspace/teams/").get(getAllPublicTeamsController)
 
 
 export default router;

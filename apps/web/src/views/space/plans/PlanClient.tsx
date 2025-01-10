cd232ea3f282bebe2224a6cce9249c7ef6abd3e4
manashbarai <manashjbarai@gmail.com>
2025-01-11 00:08:56 +0530

    feat: enhance plan client functionality

--- apps/web/src/views/space/plans/PlanClient.tsx
+++ apps/web/src/views/space/plans/PlanClient.tsx
@@ -1,49 +1,37 @@
 "use client";
 
-import { dataStore, userStore } from "@/utils/store/zustand";
-import {  Orbit } from "lucide-react";
-import { useEffect,  useState, } from "react";
+import { dataStore } from "@/utils/store/zustand";
+import { Orbit } from "lucide-react";
+import { useEffect, useRef } from "react";
 
-import { usePathname } from "next/navigation";
+import { BACKEND_URL } from "@/utils/constants/api-endpoints";
 import IssueCard from "@/components/issueCard/IsshueCard";
 
-const PlanClient = (props: { token: string; slug: string; space: string }) => {
-  
-  const pathname = usePathname();
-  const setCurrent = userStore((state) => state.setCurrent);
-
+const PlanClient = ({ token, slug }: { token: string; slug: string }) => {
   const allIssues = dataStore((state) => state.fetchAllIssues);
   const issueStatus = dataStore((state) => state.issueStatus);
   const allLinearIssues = dataStore((state) => state.allLinearIssues);
-  
-  
- 
+
+  // Using useRef to avoid re-rendering
+  const workSpaceRef = useRef<boolean>(false);
 
   useEffect(() => {
-   
-    allIssues(props.token, pathname.split("/")[2],'plan');
-  }, [pathname.split("/")[2]]);
+    const myWorkSpace = localStorage.getItem("workspace_slug") === slug;
+    workSpaceRef.current = myWorkSpace;
 
-  
+    const url =
+      token && myWorkSpace
+        ? `${BACKEND_URL}/workspaces/${slug}/issues/`
+        : `${BACKEND_URL}/public/workspaces/${slug}/issues`;
 
- 
-
-  useEffect(() => {
-    setCurrent(`${props.space}-plan`);
-  }, [props.space, setCurrent]);
-
- 
- 
-
- 
-
+    allIssues(token, url, "plan");
+  }, [token, slug]); // Removed workSpaceRef from dependencies to avoid extra re-renders
 
   return (
     <section className="h-screen flex flex-col gap-y-12 flex-grow right-0 bg-dashboard pt-8">
       <div className="mx-20">
         <h2 className="text-xl flex items-center font-medium text-focus-text-hover">
           <Orbit className="mr-2" size={20} />
-          Plan
         </h2>
         <h4 className="text-focus-text text-sm mt-2 flex items-center">
           All your work items: your playground from where you push things to be
@@ -51,13 +39,12 @@ const PlanClient = (props: { token: string; slug: string; space: string }) => {
         </h4>
       </div>
 
-      
-         
-          
-          <IssueCard token={props.token}  issue={allLinearIssues} issueStatus={issueStatus} />
-
-         
-       
+      <IssueCard
+        token={token}
+        issue={allLinearIssues}
+        issueStatus={issueStatus}
+        myWorkSpace={workSpaceRef.current} // Using ref value without causing re-render
+      />
     </section>
   );
 };

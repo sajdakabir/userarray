26e5adba1a581f8b88e53adab912d8e0e07b5a6a
manashbarai <manashjbarai@gmail.com>
2025-01-10 23:59:25 +0530

    fix : improve this page without logged in and logged in can see issue

--- apps/web/src/app/[slug]/plan/page.tsx
+++ apps/web/src/app/[slug]/plan/page.tsx
@@ -1,37 +1,17 @@
-import IssueCard from "@/components/issueCard/IsshueCard";
-import { Issue, IssueStatus } from "@/lib/types/Issue";
-import getAllIssue from "@/server/fetchers/issue/getAllLinearIssue";
-import React from "react";
-import { redirect } from "next/navigation";
+
 import { cookies } from "next/headers";
 import { ACCESS_TOKEN } from "@/utils/constants/cookie";
-import { BACKEND_URL } from "@/utils/constants/api-endpoints";
-import getPublicIssue from "@/server/fetchers/issue/getPublicIssue";
-// Dynamic SSG Page Component
+import PlanClient from "@/views/space/plans/PlanClient";
+
 const Page = async ({ params }: { params: { slug: string } }) => {
   const accessToken = cookies().get(ACCESS_TOKEN)?.value as string;
-
-  // Fetch issues with null handling
-  const all_issues = await getPublicIssue(
-    `${BACKEND_URL}/public/workspaces/${params.slug}/issues/`
-  );
-
-  if (!all_issues) return redirect("/error?status=500");
-  // Extract unique issue statuses safely
-  const uniqueIssueStatuses: IssueStatus[] = Array.from(
-    new Map(all_issues.map(({ state }) => [state.id, state])).values()
-  );
-
   return (
-    <div>
-      <h1>All Issues for {params.slug}</h1>
-
-      <IssueCard
-        issue={all_issues}
-        token={accessToken}
-        issueStatus={uniqueIssueStatuses}
+    <>
+      <PlanClient
+      token={accessToken}
+      slug={params.slug}
       />
-    </div>
+    </>
   );
 };
 

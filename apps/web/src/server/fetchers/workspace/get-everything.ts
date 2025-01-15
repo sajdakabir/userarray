2dbbe861188b388eaa34e57599fce86c57f3d3ef
sajdkabir <sajda.kbir@gmail.com>
2025-01-15 14:22:16 +0530

    build: all the server build fixed

--- apps/web/src/server/fetchers/workspace/get-everything.ts
+++ apps/web/src/server/fetchers/workspace/get-everything.ts
@@ -1,25 +1,25 @@
-import { USER_WORKSPACE } from "@/utils/constants/api-endpoints";
-import { Everything } from "@/types/Everything";
+// import { USER_WORKSPACE } from "@/utils/constants/api-endpoints";
+// import { Everything } from "@/types/Everything";
 
-const getEverything = async (token: string, slug: string) => {
-  let response: Response;
+// const getEverything = async (token: string, slug: string) => {
+//   let response: Response;
 
-  try {
-    response = await fetch(USER_WORKSPACE + `/${slug}/ping`, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    });
-    if (!response.ok) {
-      return null;
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    return null;
-  }
+//   try {
+//     response = await fetch(USER_WORKSPACE + `/${slug}/ping`, {
+//       headers: {
+//         Authorization: "Bearer " + token,
+//       },
+//     });
+//     if (!response.ok) {
+//       return null;
+//     }
+//   } catch (error) {
+//     console.error("Error:", error);
+//     return null;
+//   }
 
-  const eve: Everything = await response.json();
-  return eve;
-};
+//   const eve: Everything = await response.json();
+//   return eve;
+// };
 
-export default getEverything;
+// export default getEverything;
